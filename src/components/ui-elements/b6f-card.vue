2f07029d0b6d4bc919ac08ecd593f0ec3e005308
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-17 09:37:23 +0100

    style(ui-element): style card element

--- src/components/ui-elements/b6f-card.vue
+++ src/components/ui-elements/b6f-card.vue
@@ -1,9 +1,41 @@
 <template>
-  <div></div>
+  <div class="card">
+    <slot></slot>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+.card {
+  border-radius: 3px;
+  background-color: #ffffff;
+  box-shadow: 3px 3px 8px rgba(0, 0, 0, 0.15);
+  padding: 20px 40px;
+  display: flex;
+  justify-content: center;
+}
+
+.h-content {
+  height: unset;
+}
+.h-fixed {
+  height: 500px;
+}
+
+.w-auto {
+  width: auto;
+}
+
+.w-sm {
+  width: 200px;
+}
+.w-md {
+  width: 400px;
+}
+.w-lg {
+  width: 800px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <b6f-button size="sm">button</b6f-button>
+    <b6f-card></b6f-card>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import B6fButton from "../components/ui-elements/b6f-button.vue";
+import B6fCard from "../components/ui-elements/b6f-card.vue";
 
 export default {
   name: "Home",
   components: {
     B6fButton,
+    B6fCard,
   },
 };
 </script>

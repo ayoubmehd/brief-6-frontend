4a86557d79ab374ef7c58fecec554e8a996aab0c
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-17 09:15:36 +0100

    feat(ui-elements): create button element

---
+++ src/components/ui-elements/b6f-button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button :class="classType">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  props: {
+    size: String,
+  },
+  setup(props) {
+    const classes = ["sm", "md", "lg"];
+    const classType = computed(() =>
+      classes.indexOf(props.size) !== -1 ? props.size : "lg"
+    );
+
+    return { classType };
+  },
+};
+</script>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  background-color: #5bc0de;
+  border: none;
+  outline: none;
+  color: #fff;
+  border-radius: 3px;
+  cursor: pointer;
+}
+.lg {
+  width: 150px;
+}
+.md {
+  width: 100px;
+}
+.sm {
+  width: 50px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <b6f-button size="sm">button</b6f-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import B6fButton from "../components/ui-elements/b6f-button.vue";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    B6fButton,
+  },
 };
 </script>

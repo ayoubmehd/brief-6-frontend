1b073ea42c26174228963f0d2be359d0f2f9cac2
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-19 00:34:11 +0100

    style: build icon component

---
+++ src/components/ui-elements/b6f-icon.vue
@@ -0,0 +1,34 @@
+<template>
+  <i v-html="Icon"></i>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+import Icons from "../../assets/icons/index.js";
+
+export default {
+  name: "B6fIcon",
+  props: {
+    svg: String,
+    fill: String,
+  },
+  setup(props) {
+    const Icon = computed(() => Icons[props.svg]);
+
+    return { Icon };
+  },
+};
+</script>
+
+<style>
+i {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+svg {
+  max-width: 20px;
+  max-height: 20px;
+  fill: #fff;
+}
+</style>

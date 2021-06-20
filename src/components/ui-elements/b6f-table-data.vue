90d093190c5cd282f8e4e8991e7da4b077d0f9b0
ayoubmehd <safi-_-gio-raja@live.fr>
2021-06-20 12:39:57 +0100

    refactor: create a separate components for td and edit input

---
+++ src/components/ui-elements/b6f-table-data.vue
@@ -0,0 +1,48 @@
+<template>
+  <td>
+    <b6f-form-element
+      v-if="showInput"
+      :element="element"
+      :showLabel="false"
+      v-model="value"
+      v-bind="$attrs"
+    >
+      <option value="-1">horaire</option>
+      <option v-for="h in horaire" :value="h" :key="h">{{ h }}</option>
+    </b6f-form-element>
+    <span v-else>
+      <slot></slot>
+    </span>
+  </td>
+</template>
+
+<script>
+import { computed } from "@vue/runtime-core";
+import B6fFormElement from "./b6f-form-element.vue";
+export default {
+  components: {
+    B6fFormElement,
+  },
+  props: {
+    element: String,
+    modelValue: [String, Date, Number],
+    showInput: Boolean,
+    horaire: Array,
+  },
+  emits: [""],
+  setup(props, { emit }) {
+    const value = computed({
+      get() {
+        return props.modelValue;
+      },
+      set(val) {
+        emit("update:modelValue", val);
+      },
+    });
+
+    return { value };
+  },
+};
+</script>
+
+<style></style>
